c235133988e8d402eb7038532b98125eb278b736
patkamon <patkamon.a@ku.th>
2023-02-15 12:50:44 +0700

    initital

---
+++ src/main/java/ku/kinteow/KinteowApplication.java
@@ -0,0 +1,13 @@
+package ku.kinteow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinteowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinteowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/kinteow/KinteowApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.kinteow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinteowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
